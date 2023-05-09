d21b79cd3858c7c42ee0c8f6b013631ed24f54c5
OILEGA <117779858+OILEGA@users.noreply.github.com>
2023-05-09 21:13:37 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,1 +1,14 @@
+#!/usr/bin/python3
+from models import storage
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+  """ """
+  def __init__(self, *args, **kwargs):
+    """ Initializing our base instance 
+    *args: gives list of arguments
+    
+    
 
