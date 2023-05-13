163dfcb6a864a60f0cbdabe336df3bc97e7e940d
OILEGA <get2mena@gmail.com>
2023-05-13 13:56:28 +0100

    AirBnB Console Team Project

--- models/base_model.py
+++ models/base_model.py
@@ -10,7 +10,16 @@ class BaseModel():
         """ Initializing our base instances
         of id, created_at and updated_at
         """
-
+        if len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue:
+                elif key == "created_at" or key == "updated_at":
+                    self.__dict__[key] = datetime.fromisoformat(value):
+                else:
+                    self.__dict__[key] = value:
+        else:
+    
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
