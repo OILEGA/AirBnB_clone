be3270715e598306e452e005590b1beb4036f003
OILEGA <get2mena@gmail.com>
2023-05-13 22:34:07 +0100

    AirBnB Console Team Prject

--- models/base_model.py
+++ models/base_model.py
@@ -2,10 +2,11 @@
 
 import uuid
 from datetime import datetime
+import models
 
 
 class BaseModel():
-    
+
     def __init__(self, *args, **kwargs):
         """ Initializing our base instances
         of id, created_at and updated_at
@@ -19,18 +20,20 @@ class BaseModel():
                 else:
                     self.__dict__[key] = value
         else:
-    
+
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """ using the str to enable us print our instance and return a string """
         return f"({self.__class__.__name__}) ({self.id}) {self.__dict__}"
 
     def save(self):
-        """ updates to realtime anytime a change is made """ 
+        """ updates to realtime anytime a change is made """
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """ creating and updating a copy of our origial dictionary """
@@ -39,6 +42,4 @@ class BaseModel():
         copy_dict["created_at"] = self.created_at.isoformat()
         copy_dict["updated_at"] = self.updated_at.isoformat()
         return copy_dict
-
-
-
+        
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,1 +1,38 @@
+#!/usr/bin/python3
+import json
+from models import base_model
+
+class FileStorage():
+    """ a class that serializes json and deserializes json file 
+    to instances. We are also creating a private instance that is 
+    a string and a path to the json file """
+    
+    __file_path = "file.json"
+    __objects = {}
+
+def all(self):
+    return self.__objects
+
+def new(self, obj):
+    key = f"{obj.__class.__.__name__}.{obj.id}"
+    self.__objects[key] = obj
+    
+    """ opening the file to enable us save ojects to json file """    
+def save(self):
+    with open(self.__file_path, "w", encoding="utf-8") as f:
+        dc = {k : val.to_dict() for k, val in self.__objects.items()}
+        json.dump(dc, f) #converting to json string; k= key
+
+def reload(self):
+    try:
+        with open(self.__file_path, "r", encoding="utf-8") as f:
+            obj_dict = json.load(f)
+            for o in obj_dict.values():
+                class_name = o["__class__"]
+                del o["__class__"]
+                self.new(eval(f"base_model.{class_name}")(**o))
+
+    except FileNotFoundError:
+        return
+
 
