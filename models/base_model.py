8c9eabc28c0e1a83bfab4a9cb8badb65fe784d70
OILEGA <get2mena@gmail.com>
2023-05-13 14:26:29 +0100

    AirBnB Console Team Project

--- models/base_model.py
+++ models/base_model.py
@@ -13,16 +13,16 @@ class BaseModel():
         if len(kwargs) != 0:
             for key, value in kwargs.items():
                 if key == "__class__":
-                    continue:
-                elif key == "created_at" or key == "updated_at":
-                    self.__dict__[key] = datetime.fromisoformat(value):
+                    continue
+                elif key == "updated_at" or key == "created_at":
+                    self.__dict__[key] = datetime.fromisoformat(value)
                 else:
-                    self.__dict__[key] = value:
+                    self.__dict__[key] = value
         else:
     
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """ using the str to enable us print our instance and return a string """
